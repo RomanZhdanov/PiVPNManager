43634e0f2974cd29a52035266fa8a4eaa03da2f3
Roman Zhdanov <zhdanov.roma@gmail.com>
2023-05-26 16:11:19 +0300

    Added performance behaviour pipeline.

--- src/Application/ConfigureServices.cs
+++ src/Application/ConfigureServices.cs
@@ -15,7 +15,7 @@ namespace PiVPNManager.Application
                 cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
                 cfg.AddBehavior(typeof(IPipelineBehavior<,>), typeof(UnhandledExceptionBehaviour<,>));
                 cfg.AddBehavior(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
-
+                cfg.AddBehavior(typeof(IPipelineBehavior<,>), typeof(PerformanceBehaviour<,>));
             });
 
             return services;
